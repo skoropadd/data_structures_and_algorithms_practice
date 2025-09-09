2e73fe54710016f75e68420ab46b790c0b3d8c26
Daria <daria@Darias-MacBook-Air.local>
2025-09-09 16:35:33 +0200

    feat(arrays_hashing/0242): add valid anagram solutions (sorting + hashmap) with tests

--- leetcode/arrays_hashing/lc_0242_valid_anagram/hashmap_solution.py
+++ leetcode/arrays_hashing/lc_0242_valid_anagram/hashmap_solution.py
@@ -2,7 +2,8 @@ def is_anagram_hashmap(s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
 
-    countS, countT = {}, {}
+    countS: dict[str, int] = {}
+    countT: dict[str, int] = {}
     for i in range(len(s)):
         countS[s[i]] = 1 + countS.get(s[i], 0)
         countT[t[i]] = 1 + countT.get(t[i], 0)
